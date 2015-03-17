b788c02f1eac7cd86d01d7aabfbb763277f6faf0
Dan Chao <dan@play-i.com>
2015-03-16 22:02:34 -0700

    change factory name

--- index.js
+++ index.js
@@ -27,4 +27,4 @@ var framework = function(files) {
 };
 
 framework.$inject = ['config.files'];
-module.exports = {'framework:sinon-chai': ['factory', framework]};
+module.exports = {'framework:chai-subset': ['factory', framework]};
